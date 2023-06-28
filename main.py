fb045dc5aae61fb1fdd009b3f1ced7f1139c9e5a
Ezra Mendelson <emendel@umich.edu>
2023-06-27 21:32:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-STARTING_FLOOR = 12
-INPUT = [2,9,1,32]
 VELOCITY = 10
 
+def driver():
+  start, floors = generate_inputs()
+  return calculate_output(start,floors)
+  
+def calculate_output(start, floors):
+  total_time = 0
+  current_floor = start
+  for next_floor in floors:
+    total_time += time_to_floor(current_floor, next_floor)
+    current_floor = next_floor
+  output = [total_time] + [start] + floors
+  return output
+
 def generate_inputs():
-  print("Starting Floor? (Defaults to 12 if invalid data is given)")
-  start = input()
-  print("Comma separated value of floors to travel to. Example: 2,9,1,32 - if given data is invalid will default to example")
-  floors = input()
-  start = validate_start(start)  
+  start = int(input("Enter starting Floor : "))
+  floors = input("Comma separated value of floors to travel to. Example: 2,9,1,32 - if given data is invalid will default to example")
   floors = validate_floors(floors)
   return start, floors
 
-def validate_start(start):
-  if not isinstance(start, int) or start < 0:
-    return 12
 
 def validate_floors(floors):
   valid_input = "1234567890, "
@@ -26,20 +31,19 @@ def validate_floors(floors):
   floors = floors.split(",")
   res = [eval(i) for i in floors]
   return res
-
-def driver():
-  start, floors = generate_inputs()
-  total_time = 0
-  current_floor = start
-  for next_floor in INPUT:
-    total_time += time_to_floor(current_floor, next_floor)
-    current_floor = next_floor
-  output = [total_time] + [start] + floors
-  return(output)
    
 def time_to_floor(origin, destination):
   return(abs(origin - destination) * VELOCITY)
-  
+
+def test_validate_floors_with_empty_floors_returns_default():
+  assert validate_floors("2,9") == [2,9], "valid floors should return [2,9]"
+
+def test_validate_floors_with_empty_floors_returns_default():
+  assert validate_floors("") == [2,9,1,32], "empty floors should return default"
+
+def test_validate_floors_with_letters_returns_default():
+  assert validate_floors("23,9,mm,09") == [2,9,1,32], "letters in floors should return default" 
+
 def test_time_to_floor_same_floor_returns_0():
   assert time_to_floor(8,8) == 0, "time to floor should be 0"
 
@@ -50,6 +54,22 @@ def test_time_to_floor_returns_20():
   assert time_to_floor(8, 6) == 20, "time to floor should be 20"
 
 def test_given_example_returns_560():
-  assert driver(12, [2,9,1,32]) == [560,12,2,9,1,32], "given examples returns 560,12,2,9,1,32"
+  assert calculate_output(12, [2,9,1,32]) == [560,12,2,9,1,32], "given examples returns 560,12,2,9,1,32"
   
+  
+
+
+
+
+
+
+
+
+test_validate_floors_with_empty_floors_returns_default()
+test_validate_floors_with_empty_floors_returns_default()
+test_validate_floors_with_letters_returns_default()
+test_time_to_floor_same_floor_returns_0()
+test_time_to_floor_returns_10()
+test_time_to_floor_returns_20()
+test_given_example_returns_560()
 print(driver())
