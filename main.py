cb03bb3cc7cee4b4a5fcb58e1ecce8dd886e8bcd
Ezra Mendelson <emendel@umich.edu>
2023-06-28 18:34:35 -0400

    remove tests from main.py

--- main.py
+++ main.py
@@ -40,43 +40,6 @@ class Elevator:
     def time_to_floor(self, origin, destination):
       return(abs(origin - destination) * VELOCITY)
     
-    def test_validate_floors_with_empty_floors_returns_default():
-      assert validate_floors("2,9") == [2,9], "valid floors should return [2,9]"
-    
-    def test_validate_floors_with_empty_floors_returns_default():
-      assert validate_floors("") == [2,9,1,32], "empty floors should return default"
-    
-    def test_validate_floors_with_letters_returns_default():
-      assert validate_floors("23,9,mm,09") == [2,9,1,32], "letters in floors should return default" 
-    
-    def test_time_to_floor_same_floor_returns_0():
-      assert time_to_floor(8,8) == 0, "time to floor should be 0"
-    
-    def test_time_to_floor_returns_10():
-      assert time_to_floor(8, 9) == 10, "time to floor should be 10"
-    
-    def test_time_to_floor_returns_20():
-      assert time_to_floor(8, 6) == 20, "time to floor should be 20"
-    
-    def test_given_example_returns_560():
-      assert calculate_output(12, [2,9,1,32]) == [560,12,2,9,1,32], "given examples returns 560,12,2,9,1,32"
-      
-    
 if __name__ == "__main__":
     elevator = Elevator()
     print(elevator.driver())
-    
-    
-    
-    
-    
-
-
-# test_validate_floors_with_empty_floors_returns_default()
-# test_validate_floors_with_empty_floors_returns_default()
-# test_validate_floors_with_letters_returns_default()
-# test_time_to_floor_same_floor_returns_0()
-# test_time_to_floor_returns_10()
-# test_time_to_floor_returns_20()
-# test_given_example_returns_560()
-# print(driver())
